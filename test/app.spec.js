dfdec0b44bebca9e2c6b489251a711941136284e
Attila732 <kovacsattila478@gmail.com>
2023-05-18 14:52:28 +0200

    nos majdnem kész de a tesztek lefutnak

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,27 +1,18 @@
-describe('A calcSurface tesztje', () => {
-    it('Input 15, 15, elvárt 2827.4333882308138', () => {    
-        let actual = calcSurface(15,15);
-        expect(actual).toBeCloseTo(2827.4333882308138,2);
+describe('A getUnsuccessPercent tesztje', () => {
+    it('Input 10, 2, elvárt 20', () => {    
+        let actual = getUnsuccessPercent(10,2);
+        expect(actual).toBe(20);
     });
-    it('Input 500, 500, elvárt 3141592.653589793', () => {    
-        let actual = calcSurface(500, 500);
-        expect(actual).toBeCloseTo(3141592.653589793,2.0);
+    it('Input 33, 33, elvárt 100', () => {    
+        let actual =getUnsuccessPercent(33, 33);
+        expect(actual).toBe(100);
     });
-    it('Input 12.5, 12.5, elvárt 3141592.653589793', () => {    
-        let actual = calcSurface(12.5, 12.5);
-        expect(actual).toBeCloseTo(1963.4954084936207,2.0);
+    it('Input 55, 17, elvárt 30.909090909090907', () => {    
+        let actual = getUnsuccessPercent(55, 17);
+        expect(actual).toBe(30.909090909090907);
     });
 
      
 });
+//az értékek 100-(a kapott eredmény)= a kimenet
 
-describe('A isGoodInput() tesztjei', () => {
-    it('Számot ad az input?', () => {
-        let actual = isGoodInput('22');
-        expect(actual).toBe(true);
-    });
-    it('Ha nem szám a bemenet, akkor false-t kapunk?', () => {
-        let actual = isGoodInput('aa');
-        expect(actual).toBe(false);
-    });
-});
